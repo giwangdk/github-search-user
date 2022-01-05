3421fa0ff906e28e464a7cc2fa634be3fecb1a8f
gidwikintan <gidwikintan@gmail.com>
2022-01-05 23:50:48 +0700

    styling input search large screen

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
         </div>
       </header>
 
-      <div className="input-search bg-white p-3 rounded-2xl shadow-black-500/50 my-4 flex justify-between">
+      <div className="input-search bg-white p-2 rounded-xl sm:p-3 sm:rounded-2xl shadow-black-500/50 my-4 flex justify-between">
         <input type="text" name="" id="" placeholder="Search Github username" className="p-2 outline-blue-300" />
-        <button className="bg-blue-500 text-white font-semibold py-3 px-3 border-0 rounded-lg">Search</button>
+        <button className="bg-blue-500 text-white font-semibold py-2 px-3  sm:py-3 sm:px-4 sm:rounded-xl border-0 rounded-lg">Search</button>
       </div>
     </div>
     </div>
